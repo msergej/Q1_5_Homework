cb623d612307235dcf10b3470c8c7e20c8283be3
Sergej <msergej@mail.ru>
2022-09-18 12:27:34 +0300

    Сохранение задачи 8 практического задания к семинару 1

--- Program.cs
+++ Program.cs
@@ -1,19 +1,39 @@
 ﻿          // Выполнение практических задааний к семинарам
 
+          //  Урок 1, задача 8
+          //  Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N:
+static string Seminar_1_Task_08(int Num) {
+          // Проверка четности числа и уменьшение на 1 при необходимости
+    string ResultStr = "";
+    if ((Num % 2) != 0) Num--;
+    while (Num >= 2) {
+        if (ResultStr.Length > 0) ResultStr = ResultStr.Insert(0, ", ");
+        ResultStr = ResultStr.Insert(0, Convert.ToString(Num));
+        Num = Num - 2;
+    }
+    return ResultStr;
+}   
+          // Команды для выполнения задачи 8 (вынести вниз за тело функции и запустить на выполнение)
+Console.WriteLine("Введите натуральное число:");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"Между 1 и {N} расположены (включительно) следующие четные числа:");
+Console.WriteLine(Seminar_1_Task_08(N));
+          // Команды для выполнения задачи 8        
+
+
+/*
           //  Урок 1, задача 6
           //  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным:
 static void Seminar_1_Task_06(int Num) {
-          // Первоначальная установка максимального значения в Num1
     if ((Num % 2) == 0) Console.WriteLine($"Число {Num} четное.");
        else Console.WriteLine($"Число {Num} нечетное.");
-}   
+}  
           // Команды для выполнения задачи 6 (вынести вниз за тело функции и запустить на выполнение)
 Console.WriteLine("Введите число:");
 int N = Convert.ToInt16(Console.ReadLine());
 Seminar_1_Task_06(N);
           // Команды для выполнения задачи 6        
-
-
+*/
 /*
           //  Урок 1, задача 4
           //  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел:
@@ -23,7 +43,7 @@ static double Seminar_1_Task_04(double Num1, double Num2, double Num3) {
     if (Num2 > MaxNum) MaxNum = Num2;
        else if (Num3 > MaxNum) MaxNum = Num3;
     return MaxNum;        
-
+}
           // Команды для выполнения задачи 4 (вынести вниз за тело функции и запустить на выполнение)
 Console.WriteLine("Введите первое число:");
 double N1 = Convert.ToDouble(Console.ReadLine());
@@ -33,7 +53,6 @@ Console.WriteLine("Введите третье число:");
 double N3 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine($"Максимальное число: {Seminar_1_Task_04(N1, N2, N3)}");
           // Команды для выполнения задачи 4        
-}
 */
 /*
           //  Урок 1, задача 2
@@ -41,7 +60,7 @@ Console.WriteLine($"Максимальное число: {Seminar_1_Task_04(N1, 
 static double Seminar_1_Task_02(double Num1, double Num2) {
     if (Num1 >= Num2) return Num1;
        else return Num2;
-
+}
           // Команды для выполнения задачи 2 (вынести вниз за тело функции и запустить на выполнение)
 Console.WriteLine("Введите первое число:");
 double N1 = Convert.ToDouble(Console.ReadLine());
@@ -51,5 +70,4 @@ if (N1 == N2) Console.WriteLine($"Числа равны: {N1} = {N2}");
    else if (Seminar_1_Task_02(N1, N2) == N1) Console.WriteLine($"Большее число: {N1}, меньшее число: {N2}");
            else Console.WriteLine($"Большее число: {N2}, меньшее число: {N1}");
           // Команды для выполнения задачи 2
-}
 */
