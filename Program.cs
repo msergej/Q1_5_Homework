c4a94fff413c2f9ca16e08e994e8241686d24431
Sergej <msergej@mail.ru>
2022-09-18 08:42:11 +0300

    Сохранение задачи 4 практического задания к семинару 1

--- Program.cs
+++ Program.cs
@@ -1,23 +1,43 @@
 ﻿          // Выполнение практических задааний к семинарам
 
+
+
+/*
+          //  Урок 1, задача 4
+          //  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел:
+static double Seminar_1_Task_04(double Num1, double Num2, double Num3) {
+          // Первоначальная установка максимального значения в Num1
+    double MaxNum = Num1;
+    if (Num2 > MaxNum) MaxNum = Num2;
+       else if (Num3 > MaxNum) MaxNum = Num3;
+    return MaxNum;        
+
+          // Команды для выполнения задачи 4 (вынести вниз за тело функции и запустить на выполнение)
+Console.WriteLine("Введите первое число:");
+double N1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+double N2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+double N3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Максимальное число: {Seminar_1_Task_04(N1, N2, N3)}");
+          // Команды для выполнения задачи 4        
+}
+*/
+/*
           //  Урок 1, задача 2
           //  Напишите программу, которая на вход принимает два числа, и выдаёт, какое число большее, а какое меньшее:
 static double Seminar_1_Task_02(double Num1, double Num2) {
-    if (Num1 > Num2) return Num1;
-        else if (Num2 > Num1) return Num2;
-          // Возврат нуля, если числа равны   
-                 else return 0;        
- 
+    if (Num1 >= Num2) return Num1;
+       else return Num2;
+
           // Команды для выполнения задачи 2 (вынести вниз за тело функции и запустить на выполнение)
 Console.WriteLine("Введите первое число:");
 double N1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 double N2 = Convert.ToDouble(Console.ReadLine());
-double Max_of_two = Seminar_1_Task_02(N1, N2);
-if (Max_of_two != 0) Console.WriteLine($"Большее число: {Seminar_1_Task_02(N1, N2)}");
-    else Console.WriteLine($"Числа равны: {N1} = {N2}");
-
+if (N1 == N2) Console.WriteLine($"Числа равны: {N1} = {N2}");
+   else if (Seminar_1_Task_02(N1, N2) == N1) Console.WriteLine($"Большее число: {N1}, меньшее число: {N2}");
+           else Console.WriteLine($"Большее число: {N2}, меньшее число: {N1}");
+          // Команды для выполнения задачи 2
 }
-
-// Console.WriteLine("Hello, World!");
-// double height = Convert.ToDouble(Console.ReadLine());
+*/
